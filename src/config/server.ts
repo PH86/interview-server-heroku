6911e56441ff5f87fb912524d8b2922c6276ed86
ph86 <peter.hodgson2011@gmail.com>
2021-06-30 15:41:28 +0100

    allow all cors  test

--- src/config/server.ts
+++ src/config/server.ts
@@ -6,16 +6,18 @@ import { vacancies } from './database/vacancies';
 const app = express();
 const PORT : string|number = process.env.PORT || 5000;
 
+app.use(cors());
+
 app.get("/",(req, res) =>{
     res.send("<h1>Server Online</h1>");
 });
 
 app.listen(PORT,() => console.log(`hosting @${PORT}`));
 
-app.get('/candidates', cors(), (req, res) => {
+app.get('/candidates', (req, res) => {
     res.send(applicants)
 });
 
-app.get('/vacancies', cors(), (req, res) => {
+app.get('/vacancies', (req, res) => {
     res.send(vacancies)
 });
