b6ab8f65a7eb8decdae630ecb88c8847601e77ee
ph86 <peter.hodgson2011@gmail.com>
2021-06-30 12:27:17 +0100

    added vacancies get function

---
+++ src/config/database/vacancies.ts
@@ -0,0 +1,300 @@
+interface IVacancies {
+    id: number, 
+    title: string, 
+    company: string, 
+    location: string, 
+    salary: number, 
+    applicants: number, 
+    endDate: string,
+};
+
+export const vacancies: IVacancies[] = [{
+    id: 1,
+    title:'Software Developer',
+    company: 'Green',
+    location: 'Remote',
+    salary: 45000,
+    applicants: 51,
+    endDate: '11/11/2021'
+}, {
+    id: 2,
+    title:'Customer Service Advisor',
+    company: 'Green',
+    location: 'Newcastle',
+    salary: 25000,
+    applicants: 21,
+    endDate: '11/11/2021'
+}, {
+    id: 3,
+    title:'Chef',
+    company: 'Bistros',
+    location: 'London',
+    salary: 30000,
+    applicants: 3,
+    endDate: '11/11/2021'
+}, {
+    id: 4,
+    title:'Data Analyst',
+    company: 'BT',
+    location: 'York',
+    salary: 35000,
+    applicants: 24,
+    endDate: '11/11/2021'
+}, {
+    id: 5,
+    title:'Mechanic',
+    company: 'Daves Wheels',
+    location: 'Barnsley',
+    salary: 24000,
+    applicants: 18,
+    endDate: '11/11/2021'
+}, {
+    id: 6,
+    title:'Manager',
+    company: 'Interview',
+    location: 'Manchester',
+    salary: 60000,
+    applicants: 4,
+    endDate: '11/11/2021'
+}, {
+    id: 7,
+    title:'Software Developer',
+    company: 'Green',
+    location: 'Remote',
+    salary: 45000,
+    applicants: 15,
+    endDate: '11/11/2021'
+}, {
+    id: 8,
+    title:'Customer Service Advisor',
+    company: 'Green',
+    location: 'Newcastle',
+    salary: 25000,
+    applicants: 26,
+    endDate: '11/11/2021'
+}, {
+    id: 9,
+    title:'Chef',
+    company: 'Bistros',
+    location: 'Manchester',
+    salary: 30000,
+    applicants: 32,
+    endDate: '11/11/2021'
+}, {
+    id: 10,
+    title:'Data Analyst',
+    company: 'BT',
+    location: 'Barnsley',
+    salary: 35000,
+    applicants: 24,
+    endDate: '11/11/2021'
+}, {
+    id: 11,
+    title:'Mechanic',
+    company: 'Daves Wheels',
+    location: 'Newcastle',
+    salary: 24000,
+    applicants: 1,
+    endDate: '11/11/2021'
+}, {
+    id: 12,
+    title:'Manager',
+    company: 'Interview',
+    location: 'York',
+    salary: 60000,
+    applicants: 14,
+    endDate: '11/11/2021'
+}, {
+    id: 13,
+    title:'Software Developer',
+    company: 'BT',
+    location: 'Cornwall',
+    salary: 76000,
+    applicants: 51,
+    endDate: '11/11/2021'
+}, {
+    id: 14,
+    title:'Customer Service Advisor',
+    company: 'Bupa',
+    location: 'Immingham',
+    salary: 18000,
+    applicants: 44,
+    endDate: '11/11/2021'
+}, {
+    id: 15,
+    title:'Chef',
+    company: 'Healing Manor',
+    location: 'Healing',
+    salary: 30000,
+    applicants: 7,
+    endDate: '11/11/2021'
+}, {
+    id: 16,
+    title:'Data Analyst',
+    company: 'BT',
+    location: 'Hull',
+    salary: 35000,
+    applicants: 24,
+    endDate: '11/11/2021'
+}, {
+    id: 17,
+    title:'Mechanic',
+    company: 'Daves Wheels',
+    location: 'Barnsley',
+    salary: 24000,
+    applicants: 18,
+    endDate: '11/11/2021'
+}, {
+    id: 18,
+    title:'Manager',
+    company: 'Interview',
+    location: 'Manchester',
+    salary: 45000,
+    applicants: 4,
+    endDate: '11/11/2021'
+}, {
+    id: 19,
+    title:'Software Developer',
+    company: 'Green',
+    location: 'Remote',
+    salary: 45000,
+    applicants: 15,
+    endDate: '11/11/2021'
+}, {
+    id: 20,
+    title:'Customer Service Advisor',
+    company: 'Green',
+    location: 'Newcastle',
+    salary: 25000,
+    applicants: 26,
+    endDate: '11/11/2021'
+}, {
+    id: 21,
+    title:'Chef',
+    company: 'Bistros',
+    location: 'Manchester',
+    salary: 30000,
+    applicants: 32,
+    endDate: '11/11/2021'
+}, {
+    id: 22,
+    title:'Data Analyst',
+    company: 'BT',
+    location: 'Barnsley',
+    salary: 35000,
+    applicants: 24,
+    endDate: '11/11/2021'
+}, {
+    id: 23,
+    title:'Mechanic',
+    company: 'Daves Wheels',
+    location: 'Newcastle',
+    salary: 24000,
+    applicants: 1,
+    endDate: '11/11/2021'
+}, {
+    id: 24,
+    title:'Manager',
+    company: 'Interview',
+    location: 'York',
+    salary: 60000,
+    applicants: 14,
+    endDate: '11/11/2021'
+}, {
+    id: 25,
+    title:'Software Developer',
+    company: 'Green',
+    location: 'Remote',
+    salary: 45000,
+    applicants: 51,
+    endDate: '11/11/2021'
+}, {
+    id: 26,
+    title:'Customer Service Advisor',
+    company: 'Green',
+    location: 'Newcastle',
+    salary: 25000,
+    applicants: 21,
+    endDate: '11/11/2021'
+}, {
+    id: 27,
+    title:'Chef',
+    company: 'Bistros',
+    location: 'London',
+    salary: 30000,
+    applicants: 3,
+    endDate: '11/11/2021'
+}, {
+    id: 28,
+    title:'Data Analyst',
+    company: 'BT',
+    location: 'York',
+    salary: 35000,
+    applicants: 24,
+    endDate: '11/11/2021'
+}, {
+    id: 29,
+    title:'Mechanic',
+    company: 'Daves Wheels',
+    location: 'Barnsley',
+    salary: 24000,
+    applicants: 18,
+    endDate: '11/11/2021'
+}, {
+    id: 30,
+    title:'Manager',
+    company: 'Interview',
+    location: 'Manchester',
+    salary: 60000,
+    applicants: 4,
+    endDate: '11/11/2021'
+}, {
+    id: 31,
+    title:'Software Developer',
+    company: 'Green',
+    location: 'Remote',
+    salary: 45000,
+    applicants: 15,
+    endDate: '11/11/2021'
+}, {
+    id: 32,
+    title:'Customer Service Advisor',
+    company: 'Green',
+    location: 'Newcastle',
+    salary: 25000,
+    applicants: 26,
+    endDate: '11/11/2021'
+}, {
+    id: 33,
+    title:'Chef',
+    company: 'Bistros',
+    location: 'Manchester',
+    salary: 30000,
+    applicants: 32,
+    endDate: '11/11/2021'
+}, {
+    id: 34,
+    title:'Data Analyst',
+    company: 'BT',
+    location: 'Barnsley',
+    salary: 35000,
+    applicants: 24,
+    endDate: '11/11/2021'
+}, {
+    id: 35,
+    title:'Mechanic',
+    company: 'Daves Wheels',
+    location: 'Newcastle',
+    salary: 24000,
+    applicants: 1,
+    endDate: '11/11/2021'
+}, {
+    id: 36,
+    title:'Manager',
+    company: 'Interview',
+    location: 'York',
+    salary: 60000,
+    applicants: 14,
+    endDate: '11/11/2021'
+},
+];
--- src/config/server.ts
+++ src/config/server.ts
@@ -1,5 +1,6 @@
 import express from 'express';
 import { applicants } from './database/applicants';
+import { vacancies } from './database/vacancies';
 
 const app = express();
 const PORT : string|number = process.env.PORT || 5000;
@@ -12,4 +13,8 @@ app.listen(PORT,() => console.log(`hosting @${PORT}`));
 
 app.get('/candidates', (req, res) => {
     res.send(applicants)
+});
+
+app.get('/vacancies', (req, res) => {
+    res.send(vacancies)
 });
