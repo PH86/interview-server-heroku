c7d666ca3cc3e3b28f7d6362145ddfd41cebf117
ph86 <peter.hodgson2011@gmail.com>
2021-06-30 10:46:41 +0100

    fixed get candidates function

--- src/config/server.ts
+++ src/config/server.ts
@@ -4,8 +4,8 @@ import { applicants } from './database/applicants';
 const app = express();
 const PORT : string|number = process.env.PORT || 5000;
 
-app.use("/",(req, res) =>{
-    res.send("<h1>Welcome to your simple server! Awesome right</h1>");
+app.get("/",(req, res) =>{
+    res.send("<h1>Server Online</h1>");
 });
 
 app.listen(PORT,() => console.log(`hosting @${PORT}`));
